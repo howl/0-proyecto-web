41f2e41d65bd8a4337fc009f4e94495c4f018594
David Santamaría Rogado <howl.nsp@gmail.com>
2026-04-29 13:47:33 +0200

    move background div into javascript
    Signed-off-by: David Santamaría Rogado <howl.nsp@gmail.com>

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 'use strict';
 
-const addPageStructure = (() => {
+(() => {
+  const bodyNode = document.querySelector('body');
+  const fragmento = document.createDocumentFragment();
+  const backgroundNode = document.createElement('div');
+  backgroundNode.classList.add('background');
+  for (let i = 0; i < 20; i++)
+    backgroundNode.appendChild(document.createElement('span'));
+  fragmento.appendChild(backgroundNode);
+  console.log(fragmento);
 
+  bodyNode.prepend(fragmento);
 })();
